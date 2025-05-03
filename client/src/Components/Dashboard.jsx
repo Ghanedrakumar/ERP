15ff4fde104f1e67f93d632c44967ca51c6ded77
Ghanedra kumar <ghanendra750594@gmail.com>
2025-05-03 21:25:22 +0530

    add some element

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -94,7 +94,7 @@ const Dashboard = () => {
               </tr>
             </thead>
             <tbody>
-              {allNotes.map((note, ) => (
+              {allNotes.map((note,index ) => (
                 <tr key={note._id}>
                   <td className="border px-4 py-2">{note.name}</td>
                   <td className="border px-4 py-2">{note.grade}</td>
